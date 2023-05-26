bbb44ff5e69508bb652333f2ccb4d81574d96442
noel_biggs <noelbiggs28@gmail.com>
2023-05-25 22:04:43 -1000

    completed lesson_010

--- lesson_10/pets/pets.py
+++ lesson_10/pets/pets.py
@@ -10,29 +10,28 @@ class NeighborhoodPets:
 
     def add_pet(self, pet_name, pet_species, pet_owner_name):
         if pet_name in self._pets:
-            raise DuplicateNameError('A neighborhood pet already has that name') 
+            raise DuplicateNameError("A neighborhood pet already has that name") 
         else:
-            self._pets[pet_name] = {'species': pet_species, 'owner': pet_owner_name}
+            self._pets[pet_name] = {"species": pet_species, "owner": pet_owner_name}
 
     def delete_pet(self, pet_name):
         del self._pets[pet_name]
 
     def get_owner(self, pet_name):
-        return self._pets[pet_name]['owner']
+        return self._pets[pet_name]["owner"]
 
     def save_as_json(self, name_of_file):
-        with open(name_of_file,'w') as outfile:
-            outfile.write(str(self._pets))
+        with open(name_of_file,"w") as outfile:
+            json.dump(self._pets, outfile)
     
     def read_json(self, name_of_file):
-        with open(name_of_file, 'r') as infile:
+        with open(name_of_file, "r") as infile:
             self._pets = json.load(infile)
 
     def get_all_species(self):
         species_set = set()
         for pet in self._pets:
-            species_set.add(self._pets[pet]['species'])
-            # print(pet)
+            species_set.add(self._pets[pet]["species"])
         return species_set
 
 np = NeighborhoodPets()
@@ -41,9 +40,11 @@ try:
     np.add_pet("Tiny", "stegasaurus", "Rachel")
     np.add_pet("Spot", "zebra", "Farrokh")
 except DuplicateNameError:
-    print('You tried to enter a pet with the same name as another pet.')
+    print("You tried to enter a pet with the same name as another pet.")
 np.save_as_json("pets.json")
 np.delete_pet("Tiny")
 spot_owner = np.get_owner("Spot")
 np.read_json("other_pets.json")  # where other_pets.json is a file it saved in some previous session
+np.delete_pet("Tiny")
 species_set = np.get_all_species()
+print(species_set)
